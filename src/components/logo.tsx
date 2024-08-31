1bad6eb0e8f7baabf34f4f3615f8529349d5734b
yoko-at-home <tw107sf.ham@icloud.com>
2024-08-31 11:47:30 +0900

    favicon, sitemetadata, logo

---
+++ src/components/logo.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { siteMetadata } from "@/data/siteMetadata";
+
+export const Logo = () => {
+  return (
+    <div className="m-16">
+      <div className="balloonoya border">
+        {siteMetadata.title}
+        <span className="balloon font-semibold">🎈みんな来てね！🎈</span>
+      </div>
+      <style jsx>{`
+        .border {
+          padding: 5px 8px;
+          box-shadow: 10px 10px 11px #aaaaaa, -10px -10px 11px #ffffff;
+          color: #aaaaaa;
+          background: linear-gradient(
+            75deg,
+            rgba(2, 9, 66, 1) 4%,
+            rgba(6, 85, 92, 1) 76%
+          );
+        }
+        .balloonoya {
+          position: relative; /* 指定した分だけ相対的に移動 */
+        }
+        .balloonoya:hover .balloon {
+          display: inline; /* インライン要素として表示 */
+        }
+        .balloon {
+          position: absolute; /* 親要素を基準 */
+          display: none; /* 要素を非表示 */
+          padding: 5px 6px; /* テキストの前後の余白 */
+          background-color: rgb(233, 250, 228, 0.6); /* 背景色（透明度） */
+          width: 90px; /* 吹き出し全体の幅 */
+          right: -50%; /* 表示位置 */
+          top: 50%;
+          font-size: 70%; /* 文字サイズ */
+          color: rgb(2, 9, 66);
+          box-shadow: 5px 5px 11px rgb(109, 112, 122),
+            -5px -5px 11px rgb(109, 112, 122);
+          border-radius: 9999px;
+          text-align: center;
+        }
+      `}</style>
+    </div>
+  );
+};
---
+++ src/data/siteMetadata.ts
@@ -0,0 +1,18 @@
+export const siteMetadata = {
+  author: "Smile Marche",
+  title: "Smile Marche",
+  headerTitle: "Smile Marche",
+  description:
+    "Smile Marcheは、手作り作家さんや地域の事業者・ボランティア団体などを応援し、活躍できる場所を提供することで、お客様との交流と出会いの場として、地元活性の一助となるように地域に根ざした活動をします。",
+  language: "jp-ja",
+  siteUrl: "",
+  siteLogo: "/static/favicon_io/favicon-32x32.png",
+  image: "/static/favicon_io/android-chrome-192x192.png",
+  // socialBanner: "/static/images/twitter-card.png",
+  // ogImage: "/static/images/twitter-card.png",
+  email: "smile.hm.zama@gmail.com",
+  HP: "https://smilebliss.amebaownd.com",
+  facebook: "https://www.facebook.com/smilebliss.zama",
+  instagram: "https://www.instagram.com/smile.hm.zama",
+  locale: "ja-JP",
+};
