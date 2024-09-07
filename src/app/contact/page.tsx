ce6409d5dfe3ebee35ec08faaa18a56ceb42f2ba
yoko-at-home <tw107sf.ham@icloud.com>
2024-09-07 19:51:01 +0900

    dark mode対応

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,7 @@ import BackGround from "@/components/Background/";
 const ContactPage = () => {
   return (
     <div className="relative">
-      <div className="absolute top-1/2 right-1/2 pl-10">
+      <div className="absolute top-1/2 right-1/2 pl-10 dark:text-white">
         お問い合わせは、ページ下のSNSまたはメールにて承ります
       </div>
       <BackGround />
