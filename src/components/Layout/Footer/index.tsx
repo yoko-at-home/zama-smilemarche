cbe74c2792831a0747ccefe71d0e2aca1645733f
yoko-at-home <tw107sf.ham@icloud.com>
2024-09-07 16:48:30 +0900

    add react-icon

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -5,6 +5,7 @@ import Facebook from "@/components/icons/facebook.json";
 import Instagram from "@/components/icons/instagram.json";
 import Mail from "@/components/icons/mail.json";
 import Link from "next/link";
+import { FaHome } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -31,6 +32,14 @@ const Footer = () => {
       </nav>
 
       <div className="flex justify-center space-x-5">
+        <Link
+          href={siteMetadata.HP}
+          target="_blank"
+          className="bg-amber-50 flex flex-col items-center w-12 justify-center rounded-full text-emerald-800 animate-pulse shadow-sm"
+        >
+          <FaHome />
+          <span className="text-[1px] font-thin font-mono">Home</span>
+        </Link>
         <Link href={siteMetadata.facebook} target="_blank">
           <Lottie animationData={Facebook} className="h-12 w-12" loop={true} />
         </Link>
@@ -41,7 +50,7 @@ const Footer = () => {
           <Lottie animationData={Instagram} className="h-12 w-12" loop={true} />
         </Link>
       </div>
-      <div className="mb-2 flex space-x-2 text-sm text-gray-700">
+      <div className="mb-2 flex items-center justify-center align-middle space-x-2 text-sm text-gray-700 hover:text-red-400">
         <Link href={siteMetadata.HP} target="_blank">
           {siteMetadata.author}&nbsp;&copy;
           {`${new Date().getFullYear()}`}
