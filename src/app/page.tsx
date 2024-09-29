d3f1d5925470734496c02dd60b1cc2e968e6734c
yoko-at-home <tw107sf.ham@icloud.com>
2024-09-29 19:38:06 +0900

    文言アップデート

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,34 +67,31 @@ export default async function Home() {
             </div>
           </div>
         </div>
-        <TitleH1 text="smile座間の想い" />
+
+        <TitleH2 text="ボランティアスタッフ随時募集中！" />
         <div className={styles.border}>
-          <div className="m-3 p-3 sm:p-6">
-            <p className="mb-3">
-              Smile座間は、ハンドメイド作家として活動していく中でたくさんの作家さんと出会い、「ハンドメイドで座間をもっと盛り上げたい！」と想い、座間で活動している作家を中心に販売イベントを始めました。
-            </p>
-            <p className="mb-3">
-              第1回は座間駅前の小さなサロンをお借りして、地元作家9名で開催。
-              その際、お客様アンケートで「ぜひ定期的に開催を！」、「もっと広い所でやってほしい」との声が多く、第2回からは芹沢公園で開催しています。
-            </p>
-            第2回は無謀とも思える8月の開催でしたが、出店者さまは全部で37ブース集まり、ご来場者さまは500～600人もの方々にお越しいただく事ができました。
-            <p className="">
-              第3回も皆さまに楽しんで頂き、もっと座間を好きになるキッカケが作れたら･･･と思っております。
-              これまでマルシェ開催を通じて、様々な分野のご縁を繋ぐお手伝いをしてきました。
-              <br />
-              御社の活動などをたくさんの方々に知って頂けるよう、ぜひご協力させていただきます。
-            </p>
-          </div>
-        </div>
-        <TitleH2 text="ボランティア募集" />
-        <div className={styles.border}>
-          <div className="m-3 p-3 sm:p-6">
-            Smile Marcheは次のようなボランティアを募集しています。
-            <ol className="list-[square]">
-              <li className="ml-3">A</li>
-              <li className="ml-3">B</li>
-              <li className="ml-3">C</li>
-              <li className="ml-3">D</li>
+          <div className="m-3 p-3 sm:p-6 leading-loose">
+            イベントをお手伝いしていただけるボランティアさんを募集しています。
+            <br />
+            一緒にマルシェを楽しみながら参加して頂けたらと思います。
+            <br />
+            ボランティア参加を希望される方は、公式Instagramのメッセージ、または公式LINEにてお問い合わせくださいませ。
+            <ol className="list-[number]">
+              <li className="ml-3">
+                <span className="font-bold">学生ボランティア</span> <br />
+                ※ボランティア証明書を無料で発行します。
+                <br />
+                ・本部のテントの設営・受付などお手伝いいただきます。
+              </li>
+              <li className="ml-3">
+                <span className="font-bold">一般ボランティア</span> <br />
+                ・本部のテントの設営・受付などお手伝いいただきます。
+              </li>
+              <li className="ml-3">
+                <span className="font-bold">カメラ撮影ボランティア</span>
+                <br />
+                ・Webサイトや各種SNSへの掲載用の撮影をお願いしています。
+              </li>
             </ol>
           </div>
         </div>
