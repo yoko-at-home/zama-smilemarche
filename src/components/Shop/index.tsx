f9b7d1764c9d0be4e43bde42b8625b638fb3daef
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2024-09-30 13:23:36 +0900

    文字位置調整

--- src/components/Shop/index.tsx
+++ src/components/Shop/index.tsx
@@ -59,7 +59,7 @@ export const Shop: FC<Props> = ({ data }) => {
               className="h-12 w-12"
               loop={true}
             />
-            <span className="h-6 overflow-x-scroll w-[200px]">
+            <span className="flex justify-start h-6 overflow-x-scroll w-[200px]">
               {data.ingtagram.replace("https://www.instagram.com/", "")}
             </span>
           </span>
