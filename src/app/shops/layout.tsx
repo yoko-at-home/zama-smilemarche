01fca4a68d88e59f88b0fb068f90eb037707389f
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2024-10-16 22:33:58 +0900

    fix

--- src/app/shops/layout.tsx
+++ src/app/shops/layout.tsx
@@ -3,23 +3,23 @@ import SmileBlissImage from "@/components/Layout/Header/SmileBlissImage";
 import { NavLink } from "./NaviLink";
 
 const items = [
-  { href: "/shops/kitchencar", label: "Kitchen Car" },
-  { href: "/shops/food", label: "Food" },
-  { href: "/shops/workshop", label: "Workshop" },
-  { href: "/shops/craft", label: "Craft" },
-  { href: "/shops/other", label: "Others" },
+  { href: "/shops/kitchencar", label: "キッチンカー" },
+  { href: "/shops/food", label: "食品" },
+  { href: "/shops/workshop", label: "ワークショップ" },
+  { href: "/shops/craft", label: "クラフト" },
+  { href: "/shops/other", label: "他" },
 ];
 const NaviShops = () => {
   return (
-    <nav className="mt-60 text-slate-800">
+    <nav className="flex justify-center place-items-center items-end mt-60  mx-3 sm:mx-auto">
       {items.map(({ href, label }) => {
         return (
           <NavLink
             key={href}
             href={href}
-            activeClassName="hover:text-slate-400 font-bold"
+            activeClassName="text-orange-500 font-bold"
           >
-            <span className="rounded-t-lg px-1 sm:px-3 pt-2 sm:text-lg hover:text-slate-800 md:text-2xl bg-gradient-radial from-[#f0f0f0] to-[#aaa4a4] mr-1">
+            <span className="rounded-tl-lg px-1 sm:px-3 pt-2 text-sm sm:text-lg md:text-2xl bg-gradient-radial from-[#f0f0f0] to-[#aaa4a4] text-center h-20 sm:mr-1">
               {label}
             </span>
           </NavLink>
--- src/components/Layout/Header/SmileBlissImage.tsx
+++ src/components/Layout/Header/SmileBlissImage.tsx
@@ -1,9 +1,10 @@
+import { Logo } from "@/components/logo";
 import Image from "next/image";
 import Link from "next/link";
 
 const SmileBlissImage = () => {
   return (
-    <div className="absolute top-0  p-4 rounded-sm w-screen h-1/3 bg-cover bg-bottom">
+    <div className="absolute top-0  p-4 rounded-sm w-screen h-1/4 bg-cover bg-bottom">
       <Link href="/" className="absolute top-5 left-2">
         <Image
           alt="con"
@@ -14,6 +15,9 @@ const SmileBlissImage = () => {
           height={130}
         />
       </Link>
+      <div className="absolute bottom-5 left-[40%] md:left-[45%]">
+        <Logo />
+      </div>
     </div>
   );
 };
