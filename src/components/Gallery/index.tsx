d948d1fab0166d268075cffa008602ad34c36ecc
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2024-10-20 18:08:22 +0900

    文言とリンクの追加

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { ClipTextTitleReversed } from "../Titles/ClipTextTitle";
 import ImageGrid from "./ImageGrid";
 import ImageGridPortrait from "./ImageGridPortrait";
@@ -6,13 +7,18 @@ import { imagesLandscape, imagesPortrait } from "./images";
 const GalleryComponent = () => {
   return (
     <div className="bg-gradient-to-b from-gray-900 via-orange-600 to-gray-900  py-20">
-      <div className="flex justify-center">
+      <div className="flex flex-col items-center justify-center">
+        <ClipTextTitleReversed>Halloween Magic!!</ClipTextTitleReversed>
         <ClipTextTitleReversed>
-          暑かったけど、最高の笑顔に出会えました
+          ❤️ 暑かったけど、最高の笑顔 ❤️
         </ClipTextTitleReversed>
       </div>
       {/* ImageGridコンポーネントに画像オブジェクト配列を渡す */}
       <ImageGrid images={imagesLandscape} />
+      <ClipTextTitleReversed>
+        動画も見てね → <Link href="/about">Smile Marche</Link>
+      </ClipTextTitleReversed>
+
       <ImageGridPortrait images={imagesPortrait} />
     </div>
   );
