e795ff8d0c0160fec04c3aea221ec66430b578ff
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2024-10-21 19:36:42 +0900

    画像設定

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.microcms-assets.io",
+        port: "",
+        // pathname: "/assets/", // `account123` を正しいパスに修正
+      },
+    ],
+  },
+};
 
 export default nextConfig;
