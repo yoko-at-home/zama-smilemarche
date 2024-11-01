6bc4f567a4bb44cb5130b987d24c1537fe1139dd
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2024-11-01 20:20:23 +0900

    キーカラーの設定

--- src/data/siteMetadata.ts
+++ src/data/siteMetadata.ts
@@ -1,13 +1,13 @@
 export const siteMetadata = {
   author: "Smile bliss",
-  title: "Smile marche Zamaのご案内",
+  title: "Smile marche Zama",
   LogoTitle: "Smile marche",
   description:
     "Smile marche Zamaは、手作り作家さんや地域の事業者・ボランティア団体などを応援し、活躍できる場所を提供することで、お客様との交流と出会いの場として、地元活性の一助となるように地域に根ざした活動をします。",
   language: "jp-ja",
   siteUrl: "https://smilemarche.com",
   siteLogo: "https://smilemarche.com/static/favicon_io/favicon-32x32.png",
-  image: "/static/favicon_io/android-chrome-192x192.png",
+  image: "https://smilemarche.com/static/favicon_io/android-chrome-192x192.png",
   ogImage: "https://smilemarche.com/static/img/marche/twitter-image.png",
   openGraph: {
     card: "summary_large_image",
