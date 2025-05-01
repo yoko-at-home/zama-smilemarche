80cf10455447dcbbf31cc91bed4c6fb766f09418
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2025-05-01 11:26:29 +0900

    refactor

--- src/components/TopPage/Supporters.tsx
+++ src/components/TopPage/Supporters.tsx
@@ -2,13 +2,30 @@ import type { SupporterProps } from "@/types";
 import Image from "next/image";
 
 export default function Supporters({ banners }: SupporterProps) {
-  if (
-    !banners.supporter1 &&
-    !banners.supporter2 &&
-    !banners.supporter3 &&
-    !banners.supporter4 &&
-    !banners.supporter5
-  ) {
+  const supporters = [
+    {
+      name: banners.supporter1,
+      image: banners.supporter1_image,
+    },
+    {
+      name: banners.supporter2,
+      image: banners.supporter2_image,
+    },
+    {
+      name: banners.supporter3,
+      image: banners.supporter3_image,
+    },
+    {
+      name: banners.supporter4,
+      image: banners.supporter4_image,
+    },
+    {
+      name: banners.supporter5,
+      image: banners.supporter5_image,
+    },
+  ].filter((supporter) => supporter.name || supporter.image);
+
+  if (supporters.length === 0) {
     return null;
   }
 
@@ -16,53 +33,26 @@ export default function Supporters({ banners }: SupporterProps) {
     <div>
       <p>{banners.description}</p>
       <ul className="mx-auto px-3 my-10 text-lg">
-        <li className="list-[square] flex flex-col md:flex-row justify-between items-center mt-6">
-          {banners.supporter1 && (
-            <p className="ml-3 whitespace-nowrap mb-3">{banners.supporter1}</p>
-          )}
-          {banners.supporter1_image && (
-            <div className="w-36">
-              <Image
-                src={banners.supporter1_image.url}
-                height={banners.supporter1_image.height}
-                width={banners.supporter1_image.width}
-                alt={banners.supporter1}
-              />
-            </div>
-          )}
-        </li>
-        <li className="flex flex-col md:flex-row justify-between items-center mt-6">
-          {banners.supporter2 && (
-            <p className="ml-3 whitespace-nowrap mb-3 mt-6">
-              {banners.supporter2}
-            </p>
-          )}
-          {banners.supporter2_image && (
-            <div className="w-36">
-              <Image
-                src={banners.supporter2_image.url}
-                height={banners.supporter2_image.height}
-                width={banners.supporter2_image.width}
-                alt={banners.supporter2}
-              />
-            </div>
-          )}
-        </li>
-        <li className="flex flex-col md:flex-row justify-between items-center mt-6">
-          {banners.supporter3 && (
-            <p className="ml-3 whitespace-nowrap my-3">{banners.supporter3}</p>
-          )}
-          {banners.supporter3_image && (
-            <div className="w-36">
-              <Image
-                src={banners.supporter3_image.url}
-                height={banners.supporter3_image.height}
-                width={banners.supporter3_image.width}
-                alt={banners.supporter3 || "Supporter 3"}
-              />
-            </div>
-          )}
-        </li>
+        {supporters.map((supporter, index) => (
+          <li
+            key={`${supporter.name}-${supporter.image?.url || ""}`}
+            className="flex flex-col md:flex-row justify-between items-center mt-6"
+          >
+            {supporter.name && (
+              <p className="ml-3 whitespace-nowrap mb-3">{supporter.name}</p>
+            )}
+            {supporter.image && (
+              <div className="w-36">
+                <Image
+                  src={supporter.image.url}
+                  height={supporter.image.height}
+                  width={supporter.image.width}
+                  alt={supporter.name || `Supporter ${index + 1}`}
+                />
+              </div>
+            )}
+          </li>
+        ))}
       </ul>
     </div>
   );
